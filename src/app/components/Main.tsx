a303aac2003c3fd632b956383128d21db7d87600
GARY ROUSLTONE <gary.roulstone@repknight.com>
2022-03-26 15:45:04 +0000

    pass data and set up polling interval

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -2,17 +2,23 @@ import { useGetWeatherByCityQuery } from '../services/weather';
 import './Main.css';
 
 const Main = () => {
-  const { data, error, isLoading } = useGetWeatherByCityQuery('dublin');
-console.log(data)
+  const { data, error, isFetching } = useGetWeatherByCityQuery('dublin', { pollingInterval: 3000 });
+
   return(
     <section>
       {error ? (
         <>Oh no, there was an error</>
-      ) : isLoading ? (
+      ) : isFetching ? (
         <>Loading...</>
       ) : data ? (
         <>
-          <h1>Weather goes here</h1>
+          <h1>{data.name}</h1>
+          <ul>
+            <li>Temp: {data.main.temp}</li>
+            <li>Feels like: {data.main.feels_like}</li>
+            <li>Sunrise: {data.sys.sunrise}</li>
+            <li>Sunset: {data.sys.sunset}</li>
+          </ul>
         </>
       ) : null}
     </section>
--- src/app/services/weather.ts
+++ src/app/services/weather.ts
@@ -1,18 +1,42 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { config }                    from './config';
 
-type City = {
-    data: {
-      name: string
-    }
+type Weather = {
+    name: string,
+    main: {
+      feels_like: number,
+      temp: number,
+      humidity: number
+    },
+    sys: {
+      sunrise: number,
+      sunset: number
+    },
+    weather: Object
 }
+
 export const weatherApi = createApi({
   reducerPath: 'weatherApi',
   baseQuery: fetchBaseQuery({ baseUrl: config.BASE_URL }),
   endpoints: (builder) => ({
-    getWeatherByCity: builder.query<City, string>({
+    getWeatherByCity: builder.query<Weather, string>({
       query: city => `/weather?q=${city}&appid=${process.env.REACT_APP_API_URL}`,
-      transformResponse: (response:any) => response,
+      transformResponse: (response:Weather) => {
+        const { name, main, sys, weather } = response;
+        return {
+          'name': name,
+          'main': {
+            'feels_like': main.feels_like,
+            'temp': main.temp,
+            'humidity': main.humidity
+          },
+          'sys': {
+            'sunrise': sys.sunrise,
+            'sunset': sys.sunset
+          },
+          'weather': weather
+        }
+      }
     }),
   }),
 })
